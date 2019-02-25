5baf686259aeab45b10a78e4b8a266fe187f823e
Fernando Tadashi Akimoto <f.tadashi@gmail.com>
2019-02-24 23:22:15 -0300

    Fixed version dependencies.

--- finding_donors/setup-conda-environment.sh
+++ finding_donors/setup-conda-environment.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Setup Conda Environment"
-conda create -n finding_donors python==3.7.2 scikit-learn==0.17.1 scipy pandas matplotlib jupyter
+conda create -n finding_donors python=2.7 scikit-learn==0.17 scipy pandas matplotlib jupyter
